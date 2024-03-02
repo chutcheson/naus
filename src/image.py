c41c93349bdbd9ece69c6f5a3c7a4d58153b692c
Campbell Hutcheson <nchutcheson@gmail.com>
2024-03-02 13:31:02 -0500

    starting

--- src/image.py
+++ src/image.py
@@ -1,6 +1,8 @@
-from PIL import Image
 import io
 import base64
+import os
+
+from PIL import Image
 
 class CivImage:
     def __init__(self, image, description):
@@ -8,11 +10,17 @@ class CivImage:
         self.image = image
         self.downsampled_image = self.downsample_image(image)
         self.downsampled_image_base64 = self.image_to_base64(self.downsampled_image)
-
+    
     def downsample_image(self, image, size=(128, 128)):
         return image.resize(size, Image.ANTIALIAS)
 
     def image_to_base64(self, image):
-        buffered = io.BytesIO()
+        buffered = io.BytesIO() 
         image.save(buffered, format="PNG")
         return base64.b64encode(buffered.getvalue()).decode()
+
+    def save(self, file_name, folder="../data"):
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        file_path = os.path.join(folder, file_name)
+        self.downsampled_image.save(file_path)
